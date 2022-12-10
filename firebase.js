ab64da3e874e375befb8e96d615a1f40e186b760
abraao.paiva <abraao.paiva@rehagro.edu.br>
2022-12-10 09:35:00 -0300

    first commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBez_i__NODsC9a7eBZZxfV1LFCGbITw4o",
+  authDomain: "trabalho-montanha-97bae.firebaseapp.com",
+  projectId: "trabalho-montanha-97bae",
+  storageBucket: "trabalho-montanha-97bae.appspot.com",
+  messagingSenderId: "90252391773",
+  appId: "1:90252391773:web:f28da917e554eda356a4b4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
